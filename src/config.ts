f5975dcc485d7d1b4390bd59dcc872513e84c765
DigitalEngineer <kabuochijiokewilliam.u@hotmail.com>
2025-01-02 18:26:55 +0100

    fix: fixed prod server shutdown when inactive

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
     port: process.env.PORT || 3001,
     nodeEnv: process.env.NODE_ENV || 'development',
-    serverURL: process.env.SERVER_URL || 'http://localhost:3001',
+    serverURL: process.env.SERVER_URL || 'https://good-morning-api.onrender.com/',
 }
